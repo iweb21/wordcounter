e5ae9669796535e026889331cb2544ab65c3921e
Sumera Imran <159164615+iweb21@users.noreply.github.com>
2024-04-18 18:52:29 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer : {
+    Sentence:string
+} = await inquirer.prompt
+([
+    {
+            type: "input",
+            name: "Sentence",
+            message: "Enter your sentence to count the word:"
+        
+     }
+
+]);
+
+const words = answer.Sentence.trim().split(" ")
+
+console.log(`Your sentence word count is:,${words.length}`)
+
+console.log(words)
+
+
+
